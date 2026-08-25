18aa2872dbc1e6e7335e50b8f6783c68fdc3e71f
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-25 17:44:56 +0530

    done

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int line = Integer.parseInt(br.readLine().trim());
-        Function<Integer, Integer> f = x -> (x * x) + 1;
-        System.out.println(f.apply(line));
+        int[] line = Arrays.stream(br.readLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int sum = Arrays.stream(line).filter(n -> n % 2 == 0).map(n -> n * n).sum();
+        System.out.println(sum);
     }
 }
