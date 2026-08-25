9db7b2c9f6b4310d8d2f39d7eccec2e729ba26a9
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-25 18:30:30 +0530

    done

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
 import java.util.*;
 import java.io.*;
-import java.util.function.Function;
 
 public class Main {
+
+    static int divide(int a, int b) {
+        if(b == 0) throw new ArithmeticException("divide by zero");
+        return a / b;
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] line = Arrays.stream(br.readLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int sum = Arrays.stream(line).filter(n -> n % 2 == 0).map(n -> n * n).sum();
-        System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        try {
+            System.out.println("result: " + divide(a, b));
+        } catch (ArithmeticException e) {
+            System.out.println("error: " + e.getMessage());
+        }
     }
 }
