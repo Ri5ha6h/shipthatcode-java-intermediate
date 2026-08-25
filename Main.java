830bfe409d029d6cd3ae5b4ffa2279033026cf9f
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-25 17:27:07 +0530

    done

--- Main.java
+++ Main.java
@@ -1,17 +1,12 @@
 import java.util.*;
 import java.io.*;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = br.readLine().trim().split(" ");
-        Map<String, Integer> map = new HashMap<>();
-        for(String s : line){
-            map.merge(s, 1, Integer::sum);
-        }
-        Map<String, Integer> map2 = new TreeMap<>(map);
-        for(Map.Entry<String, Integer> entry : map2.entrySet()){
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+        int line = Integer.parseInt(br.readLine().trim());
+        Function<Integer, Integer> f = x -> (x * x) + 1;
+        System.out.println(f.apply(line));
     }
 }
