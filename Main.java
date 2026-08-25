09a8eae45ab836bea6c6f0766d4fb363df25773b
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-25 16:40:35 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    // class Pair<A, B> { ... }
+
+    public static void main(String[] args) {
+        // Pair<String, Integer> p = new Pair<>("Ada", 36);
+        // System.out.println(p);
+    }
+}
