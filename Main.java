1be0751a49dc7f8a1a329b1e1aebd4b1264f9ab0
Ri5ha6h <workwithmalik97@gmail.com>
2026-08-25 16:54:20 +0530

    done

--- Main.java
+++ Main.java
@@ -1,8 +1,20 @@
 public class Main {
-    // class Pair<A, B> { ... }
+     static class Pair<A, B> {
+         private A first;
+         private B second;
+
+         Pair(A first, B second) {
+             this.first = first;
+             this.second = second;
+         }
+
+         public String toString() {
+             return "(" + first + ", " + second + ")";
+         }
+     }
 
     public static void main(String[] args) {
-        // Pair<String, Integer> p = new Pair<>("Ada", 36);
-        // System.out.println(p);
+        Pair<String, Integer> p = new Pair<>("Ada", 36);
+        System.out.println(p.toString());
     }
 }
